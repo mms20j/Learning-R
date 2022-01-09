4d8a5b639e94f4084f6d1a92b70fbe5fa7fae9bb
Mohsin Sattar <mms.20j@gmail.com>
2022-01-09 22:21:25 +0500

    adding new file

---
+++ StartingR.r
@@ -0,0 +1,9 @@
+# this is my first practice of connecting RStudio with Github
+name = "Mohsin"
+academy = "Alnafi"
+Tutor = "Sana Rasheed"
+course = "R programming"
+print("About Me:")
+print("My name is " name  "\n and i am learning "  course  "\n from "  academy)
+
+# this line has some error :-)
